87ba0965cb463bf85f4afcc0d20befa926e7e9c1
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2023-09-26 20:32:47 +0530

    fix: stack page

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -1,12 +1,7 @@
 import { genPageMetadata } from 'app/seo'
-import { ProjectItem } from '@/components/ProjectItem'
-import { Proof } from '@/components/Proof'
 import { cx } from 'lib/utils'
-import Link from 'next/link'
-import Image from 'next/image'
 import { StackOdd } from '@/components/StackOdd'
 import { StackEven } from '@/components/StackEven'
-import { LogoTooltipURL } from '@/components/LogoTooltipURL'
 import { LogoTooltip } from '@/components/LogoTooltip'
 
 export const metadata = genPageMetadata({ title: 'Stack' })
@@ -22,54 +17,80 @@ export default function Page() {
 
           <div className="pt-4 grid grid-cols-1 gap-0">
             <StackOdd tag="Coding">
-              <LogoTooltip tag="VS Code" />
-              <LogoTooltip tag="Github Copilot" />
-              <LogoTooltipURL
+              <LogoTooltip
+                tag="VS Code"
+                src="https://asset.brandfetch.io/idUz6LJMxo/idNTHTS6o3.svg"
+              />
+              <LogoTooltip
+                tag="Github Copilot"
+                src="https://asset.brandfetch.io/idZAyF9rlg/id6a3YYV60.svg"
+              />
+              <LogoTooltip
                 tag="Linear"
                 src="https://asset.brandfetch.io/iduDa181eM/id0tTqetsg.svg"
               />
             </StackOdd>
             <StackEven tag="OSX">
-              <LogoTooltipURL
+              <LogoTooltip
                 tag="Raycast"
                 src="https://asset.brandfetch.io/idxxHV50Mm/idw44lawVc.svg"
               />
-              <LogoTooltip tag="GitKraken" />
-              <LogoTooltipURL
+              <LogoTooltip
+                tag="GitKraken"
+                src="https://asset.brandfetch.io/idzGznS_EP/idgLZ_0IgA.svg"
+              />
+              <LogoTooltip
                 tag="Barrier"
                 src="https://dl.flathub.org/repo/appstream/x86_64/icons/128x128/com.github.debauchee.barrier.png"
               />
-              <LogoTooltip tag="iTerm2" />
+              {/* <LogoTooltip
+                tag="iTerm2"
+                src="https://asset.brandfetch.io/iduDa181eM/id0tTqetsg.svg"
+              /> */}
             </StackEven>
             <StackOdd tag="Extensions">
-              <LogoTooltip tag="Bitwarden" />
-              <LogoTooltip tag="Grammarly" />
+              <LogoTooltip
+                tag="BitWarden"
+                src="https://asset.brandfetch.io/idoK_yj68K/idikG5ODgv.svg"
+              />
+              <LogoTooltip
+                tag="Grammarly"
+                src="https://asset.brandfetch.io/iddd_fYAYR/id1xynQtR_.svg"
+              />
             </StackOdd>
             <StackEven tag="Web Tools">
-              <LogoTooltip tag="Notion" />
-              <LogoTooltip tag="AirTable" />
-              <LogoTooltipURL
-                tag="Miro"
-                src="https://asset.brandfetch.io/idAnDTFapY/idkwvxShC9.jpeg"
+              <LogoTooltip
+                tag="Notion"
+                src="https://asset.brandfetch.io/idPYUoikV7/idKNKdmaQM.svg"
               />
-              <LogoTooltip tag="Figma" />
+              <LogoTooltip
+                tag="AirTable"
+                src="https://asset.brandfetch.io/iddsnRzkxS/iddyj0wl13.svg"
+              />
+              <LogoTooltip tag="Miro" src="https://asset.brandfetch.io/idAnDTFapY/idG4aRyg5R.svg" />
+              <LogoTooltip
+                tag="Figma"
+                src="https://asset.brandfetch.io/idZHcZ_i7F/idcJ00e7RI.svg"
+              />
             </StackEven>
             <StackOdd tag="Productivity">
-              <LogoTooltip tag="Blinkist" />
-              <LogoTooltipURL
-                tag="Substack"
-                src="https://asset.brandfetch.io/id_V6qzrDH/idcXnLQkqq.png"
+              <LogoTooltip
+                tag="Blinkist"
+                src="https://asset.brandfetch.io/iddk_TjyfQ/idBwwo_sko.svg"
               />
-              <LogoTooltip tag="Grammarly" />
+              <LogoTooltip
+                tag="Substack"
+                src="https://asset.brandfetch.io/idPmHT2Ru4/id1ffrgTAB.svg"
+              />
             </StackOdd>
             <StackEven tag="Academic">
-              <LogoTooltipURL
+              <LogoTooltip
                 tag="Overleaf"
-                src="https://asset.brandfetch.io/idtZzCB9A3/ido7H3x6Zc.png"
+                src="https://asset.brandfetch.io/idtZzCB9A3/iddgHBzIug.svg"
               />
-              <LogoTooltipURL
+              <LogoTooltip
                 tag="TurnItIn"
-                src="https://asset.brandfetch.io/idKaAfBKjS/idKK2OHDyt.jpeg"
+                src="https://asset.brandfetch.io/idKaAfBKjS/idWQWJfReW.svg"
               />
             </StackEven>
           </div>
--- components/LogoTooltip.tsx
+++ components/LogoTooltip.tsx
@@ -1,60 +1,33 @@
+'use client'
 import Image from 'next/image'
+import * as Tooltip from '@radix-ui/react-tooltip'
 
 interface LogoTooltipProps {
   tag?: string
-  description?: string
-  children?: React.ReactNode
+  src?: string
 }
-export const LogoTooltip: React.FC<LogoTooltipProps> = ({
-  children,
-  tag = '',
-  description = tag || '',
-}) => (
+export const LogoTooltip: React.FC<LogoTooltipProps> = ({ tag = 'tag', src = 'src' }) => (
   <div className="flex flex-col items-center group">
-    <Image
-      className="col-span-2 max-h-4 w-full object-contain lg:col-span-1 fill-cyan-500 hover:fill-cyan-700"
-      src={
-        'https://img.icons8.com/' +
-        isColor(tag!).toLowerCase() +
-        getCleanTag(tag!).replace(/ /g, '')?.toLowerCase()
-      }
-      alt={tag}
-      width="158"
-      height="48"
-    />
-    <div className="absolute bottom-0 flex flex-col items-center hidden mb-6 group-hover:flex">
-      <span className="relative p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">
-        {description}
-      </span>
-      <div className="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
-    </div>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <button className="IconButton">
+            <Image
+              className="col-span-2 max-h-4 w-full object-contain lg:col-span-1 dark:grayscale dark:invert"
+              src={src}
+              alt={tag}
+              width="158"
+              height="48"
+            />
+          </button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content className="bg-black px-2 py-1 text-sm text-white" sideOffset={5}>
+            {tag}
+            <Tooltip.Arrow className="TooltipArrow" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   </div>
 )
-
-function getCleanTag(tag: '' | string) {
-  switch (tag) {
-    case 'VS Code':
-      return 'visual-studio-code-2019'
-    case 'Github Copilot':
-      return 'github'
-    case 'GitKraken':
-      return 'git'
-    case 'iTerm2':
-      return 'console'
-    default:
-      return tag
-  }
-  throw new Error('Function not implemented.')
-}
-
-function isColor(tag: '' | string) {
-  switch (tag) {
-    case 'AirTable':
-      return '/'
-    case 'Blinkist':
-      return 'fluency/'
-    default:
-      return 'color/'
-  }
-  throw new Error('Function not implemented.')
-}
